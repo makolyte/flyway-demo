2183368e246c7252adcb3b435e5b8ee9ef65c9a6
unknown <yasmin.rodriguez@gmail.com>
2021-03-03 08:12:30 -0500

    initial project setup

---
+++ src/main/java/com/makolyte/flywayerrors/FlywayErrorsApplication.java
@@ -0,0 +1,13 @@
+package com.makolyte.flywayerrors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayErrorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayErrorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makolyte/flywayerrors/FlywayErrorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makolyte.flywayerrors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayErrorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
